ec3f987b1ad808aa154813963af6f96842338a5a
sademarquez <151792037+sademarquez@users.noreply.github.com>
2025-05-21 17:29:17 -0500

    Add files via upload

---
+++ scritp.js
@@ -0,0 +1,42 @@
+[
+  {
+    "id": 1,
+    "nombre": "Aguardiente Antioqueño",
+    "descripcion": "Botella de 750ml, sabor tradicional, ideal para tus fiestas.",
+    "precio": "50.000 COP",
+    "imagen": "img/licores/aguardiente-antioqueno.jpg",
+    "whatsapp_link": "https://wa.me/TUNUMERODEWHATSAPP?text=Hola!%20Estoy%20interesado%20en%20Aguardiente%20Antioque%C3%B1o"
+  },
+  {
+    "id": 2,
+    "nombre": "Ron Medellín 8 Años",
+    "descripcion": "Un ron añejo con notas de madera y vainilla, perfecto para compartir.",
+    "precio": "75.000 COP",
+    "imagen": "img/licores/ron-medellin.jpg",
+    "whatsapp_link": "https://wa.me/TUNUMERODEWHATSAPP?text=Hola!%20Estoy%20interesado%20en%20Ron%20Medellin%208%20Anos"
+  },
+  {
+    "id": 3,
+    "nombre": "Cerveza Poker Lata",
+    "descripcion": "Pack de 6 latas de 330ml, la cerveza colombiana favorita.",
+    "precio": "18.000 COP",
+    "imagen": "img/licores/poker-lata.jpg",
+    "whatsapp_link": "https://wa.me/TUNUMERODEWHATSAPP?text=Hola!%20Estoy%20interesado%20en%20Cerveza%20Poker%20Lata%20(6%20pack)"
+  },
+  {
+    "id": 4,
+    "nombre": "Vodka Smirnoff Red",
+    "descripcion": "Botella de 750ml, el vodka clásico para tus cócteles.",
+    "precio": "60.000 COP",
+    "imagen": "img/licores/vodka-smirnoff.jpg",
+    "whatsapp_link": "https://wa.me/TUNUMERODEWHATSAPP?text=Hola!%20Estoy%20interesado%20en%20Vodka%20Smirnoff%20Red"
+  },
+  {
+    "id": 5,
+    "nombre": "Whisky Old Parr 12 Años",
+    "descripcion": "Botella de 750ml, un whisky escocés suave y distintivo.",
+    "precio": "180.000 COP",
+    "imagen": "img/licores/old-parr.jpg",
+    "whatsapp_link": "https://wa.me/TUNUMERODEWHATSAPP?text=Hola!%20Estoy%20interesado%20en%20Whisky%20Old%20Parr%2012%20Anos"
+  }
+]
